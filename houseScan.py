9e4037a0acecaa9731adb752e81a9fbfb2bc9574
Eduardo Fares <edufarest@gmail.com>
2020-01-03 20:05:13 -0500

    Login and check for connected devices

--- houseScan.py
+++ houseScan.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-import csv, os, time, requests, re
+import csv, os, sys, time, requests, re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,10 +9,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-nmap = "echo ''" #"sudo nmap -sn 192.168.1.1/24 | grep -o -E '([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])'"
+if len(sys.argv) > 0:
+    if sys.argv[1] == "-h":
+        print("-h: help\n" +
+              "-f: fast, use default options")
+        sys.exit()
+
+    elif sys.argv[1] == "-f":
+        SentryMode = True
+        ScanType = 1
+else:
 
 
-SentryMode = input("Sentry Mode? [y/N]") == "y"
+    nmap = "echo ''" #"sudo nmap -sn 192.168.1.1/24 | grep -o -E '([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])'"
+
+
+    SentryMode = input("Sentry Mode? [y/N]") == "y"
+    ScanType = input("nmap(0), router(1), both(2) - default: 1") or 1
 rate = 15
 
 # Get input
@@ -24,103 +37,71 @@ cookies = {}
 
 while True:
 
-    with open('targets.csv') as targetsFile:
-        targets = csv.reader(targetsFile, delimiter=',')
+    if ScanType == 0 or ScanType == 2:
 
-        connected = os.popen(nmap).read().split('\n')
+        with open('targets.csv') as targetsFile:
+            targets = csv.reader(targetsFile, delimiter=',')
 
-
-
-        for target in targets:
-            # print(device)
-            for device in connected:
-                if str(device) == target[1]:
-                    print(target[0])
-        
-    with open('targetsName.csv') as targetsFile:
-        targets = csv.reader(targetsFile, delimiter=',')
-
-        URL = "http://192.168.1.1"
-
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-
-        driver = webdriver.Chrome(options=chrome_options)
-
-        driver.get(URL)
-
-        print(driver.get_cookies())
-
-        cookie_header = ""
-
-        for cookie in driver.get_cookies():
-            cookie_header += cookie['name'] + "=" + cookie['value'] + ";"
-
-        # POST to login
-        post = requests.post(
-            URL+"/check.php",
-            data={'username':username, 'password':password},
-            headers={"Host": "192.168.1.1",
-                     "Accept": "*/*",
-                     "Content-Type": "application/x-www-form-urlencoded",
-                     "Referer": "http://192.168.1.1/check.php",
-                     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36",
-                     "Cache-Control": "no-cache",
-                     "Accept-Encoding": "gzip, deflate",
-                     "Connection": "keep-alive",
-                     "Cookie": cookie_header}
-        )
-
-        print("Response: " + str(post.status_code))
-
-        print(post.text)
-
-        for cookie in post.cookies:
-            print("Cookie" + str(cookie))
-            cookies[cookie.name] = cookie.value
-
-        # if 'PHPSESSID' in cookies:
-        #     print(cookies['PHPSESSID'])          # <- find python equivalent to default a dict
-        # else:
-        #     print(cookies)
-
-        # connected_devices = requests.get(
-        #     URL + "/connected_devices_computers.php"
-        # )
-        #
-        # resText = connected_devices.text
-        #
-        # resHtml = BeautifulSoup(resText, 'html.parser')
-
-
-        driver.get(URL+"/connected_devices_computers.php")
-
-        online_devices = driver.find_element_by_id("online-private")
-        print(online_devices)
-        # online_devices = resHtml.findAll(id="online-private")
-
-        # print(online_devices)
+            connected = os.popen(nmap).read().split('\n')
 
 
 
-        r = requests.get(url = URL)
+            for target in targets:
+                # print(device)
+                for device in connected:
+                    if str(device) == target[1]:
+                        print(target[0])
 
-        data = r.text
+    if ScanType == 1 or ScanType == 2:
+        with open('targetsName.csv') as targetsFile:
+            targets = csv.reader(targetsFile, delimiter=',')
 
-        root = BeautifulSoup(data, 'html.parser')
+            URL = "http://192.168.1.1"
 
-        for line in targets:
-            for target in line:
+            chrome_options = Options()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
 
-                name = re.compile(target+'([\s]|$)') # Match name ending with , or eof
+            driver = webdriver.Chrome(options=chrome_options)
 
-                if name.search(data):
-                    print(target)
+            driver.get(URL)
+
+
+            cookie_header = ""
+
+            for cookie in driver.get_cookies():
+                cookie_header += cookie['name'] + "=" + cookie['value'] + ";"
+
+
+            # Fill login form
+
+            driver.find_element_by_id("username").send_keys(username)
+            driver.find_element_by_id("password").send_keys(password)
+
+            elements = driver.find_element_by_class_name("btn")
+
+            if elements:
+                elements.click()
+
+            driver.get(URL+"/connected_devices_computers.php")
+
+            online_devices = driver.find_element_by_id("online-private")
+
+            if online_devices:
+
+                connected = online_devices.find_elements_by_tag_name("u")
+
+                targets = list(targets)[0]
+
+                for target in targets:
+
+                    for device in connected:
+                        if target == device.text:
+                            print(target)
 
     if not SentryMode:
         break
-    
+
     time.sleep(rate)
 
